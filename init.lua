fb3b72cefa09ee048d1689f666934d3decc75d95
Ross Andrews <ross.andrews@gmail.com>
2015-05-19 23:35:54 -0500

    Initial commit

---
+++ init.lua
@@ -0,0 +1,69 @@
+package.path = package.path .. ";" .. os.getenv("HOME") .. "/.lua/?.lua"
+package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/.lua/?.so"
+inspect = require 'inspect'
+class = require 'middleclass'
+lpeg = require 'lpeg'
+re = require 're'
+
+-- Some table utilities
+setmetatable(table,
+             { __call = function(_, ...)
+                   return setmetatable({...}, table.mt) end })
+
+table.mt = {
+    __index = table,
+    __tostring = function(self)
+        return "{" .. self:map(tostring):concat(", ") .. "}"
+    end
+}
+
+function table:map(fn, ...)
+    local t = table()
+    for _, e in ipairs(self) do
+        local r = fn(e, ...)
+        t:insert(r)
+    end
+    return t
+end
+
+function table:select(query)
+    local t = table()
+
+    self:map( function(el)
+            if query(el) then t:insert(el) end
+    end)
+
+    return t
+end
+
+function table:reduce(fn, init)
+    local i = 1
+    local accum = init
+    if init == nil then
+        accum = self[1]
+        i = 2
+    end
+
+    while i <= #self do
+        accum = fn(accum, self[i])
+        i = i + 1
+    end
+
+    return accum
+end
+
+function table:sum()
+    return self:reduce( function(a,b) return a+b end, 0 )
+end
+
+function table:max(comp)
+    return self:reduce( function(a,b)
+            if a > b then return a
+            else return b end end )
+end
+
+function table:min(comp)
+    return self:reduce( function(a,b)
+            if a < b then return a
+            else return b end end )
+end
